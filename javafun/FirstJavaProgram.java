0d9f07a0338aec117c4ba1a56355169647fee919
James Wong <jamesrwong310@gmail.com>
2021-07-31 16:37:13 -0700

    JAVA

---
+++ javafun/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram{
+  public static void main(String[] args){
+    System.out.println("Hello my name is James");
+    System.out.println("I am 28 years old");
+    System.out.println("My hometown is Seattle, WA");
+  }
+}
